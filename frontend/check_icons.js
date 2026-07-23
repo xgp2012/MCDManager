f7856cea25b2618eebe67218d577042a144b67fd
xgp2012 <2820567502@qq.com>
2026-07-23 12:35:50 +0800

    chore: remove temp test files

--- frontend/check_icons.js
+++
@@ -1,7 +0,0 @@
-var icons = Object.keys(require('@ant-design/icons-vue'));
-var keywords = ['server', 'hdd', 'cluster', 'desktop', 'database', 'cloud', 'computer'];
-var filtered = icons.filter(function(k) {
-  var lower = k.toLowerCase();
-  return keywords.some(function(kw) { return lower.indexOf(kw) >= 0; });
-});
-console.log(filtered.sort().join('\n'));
--- frontend/check_icons.mjs
+++
@@ -1,3 +0,0 @@
-import * as Icons from '@ant-design/icons-vue';
-const names = Object.keys(Icons).filter(k => k.endsWith('Outlined')).sort();
-console.log(names.join('\n'));
